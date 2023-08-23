76f5444e42fd34e2897886d0cf2a605e5d43ff2b
Paulo Augusto Hahn <paulo.hahn@hotmail.com>
2023-08-23 22:09:25 +0200

    Created read - update - delete

--- src/main/java/com/algaworks/traffic/controllers/OwnerController.java
+++ src/main/java/com/algaworks/traffic/controllers/OwnerController.java
@@ -2,7 +2,6 @@ package com.algaworks.traffic.controllers;
 
 
 import com.algaworks.traffic.entities.Owner;
-import com.algaworks.traffic.repositories.OwnerImpl;
 import com.algaworks.traffic.services.OwnerService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,32 +13,43 @@ import java.util.List;
 
 @AllArgsConstructor
 @RestController()
+@RequestMapping("/owners")
 public class OwnerController {
 
     private OwnerService ownerService;
 
-    @GetMapping("/owners")
+    @GetMapping()
     public ResponseEntity<List<Owner>> findAll(){
         List<Owner> ownersList = ownerService.findAll();
         return ResponseEntity.ok(ownersList);
     }
 
-    @GetMapping("/owners/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Owner> findById(@PathVariable Long id){
-        Owner owner = ownerService.findById(id).orElseThrow();
-        return ResponseEntity.accepted().body(owner);
+        return ownerService.findById(id);
     }
 
-   @GetMapping("/owners/name")
+   @GetMapping("/name")
    public ResponseEntity<List<Owner>> findByNameContaining(@RequestParam String name){
         List<Owner> ownerList = ownerService.findByNameContaining(name);
         return ResponseEntity.ok(ownerList);
     }
 
-    @PostMapping("/owners")
+    @PostMapping()
     public ResponseEntity<Owner> insert(@RequestBody Owner owner){
         ownerService.insert(owner);
         return ResponseEntity.status(HttpStatus.CREATED).body(owner);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Owner> update(@RequestBody Owner owner,@PathVariable Long id){
+        return ownerService.update(owner,id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        return ownerService.delete(id);
     }
 
 }
--- src/main/java/com/algaworks/traffic/services/OwnerService.java
+++ src/main/java/com/algaworks/traffic/services/OwnerService.java
@@ -3,10 +3,11 @@ package com.algaworks.traffic.services;
 import com.algaworks.traffic.entities.Owner;
 import com.algaworks.traffic.repositories.OwnerImpl;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -14,22 +15,53 @@ public class OwnerService{
 
     private OwnerImpl ownerImpl;
 
+    @Transactional(readOnly = true)
     public List<Owner> findAll(){
         return ownerImpl.findAll();
     }
 
-    public Optional<Owner> findById(Long id){
-        return ownerImpl.findById(id);
+    @Transactional(readOnly = true)
+    public ResponseEntity<Owner> findById(Long id){
+        if(!ownerImpl.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }
+        Owner owner = ownerImpl.findById(id).get();
+
+        return ResponseEntity.ok(owner);
     }
 
+    @Transactional(readOnly = true)
     public List<Owner> findByNameContaining(String name){
         return ownerImpl.findByNameContaining(name);
     }
 
+
+    @Transactional
     public Owner insert(Owner owner){
         return ownerImpl.save(owner);
     }
 
+
+    @Transactional
+    public ResponseEntity<Owner> update(Owner owner, Long id){
+        if(!ownerImpl.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }
+        owner.setId(id);
+        Owner ownerUpdate = ownerImpl.save(owner);
+        return ResponseEntity.ok(ownerUpdate);
+    }
+
+    @Transactional
+    public ResponseEntity<Void> delete(Long id){
+
+        if(!ownerImpl.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }
+        Owner owner = ownerImpl.findById(id).get();
+        ownerImpl.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 
 
 }
